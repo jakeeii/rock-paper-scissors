5d50232666137e3c37496db6a1f6c98920e4c5de
Jake <jakevisc@gmail.com>
2022-03-28 16:54:56 -0230

    Create player select function
    Created a function that allows the game to get input from the player,
    prompting them to choose either rock, paper, or scissors.

--- rps.js
+++ rps.js
@@ -6,7 +6,18 @@
 //Display game over and show the winner
 
 const choices = ["rock", "paper", "scissors"]
+let playerSelection = playerSelect();
+let computerSelection = computerSelect();
 
 function computerSelect() {
   return choices[Math.floor(Math.random() * choices.length)]
+}
+
+function playerSelect() {
+  let input = window.prompt("Please choose either 'rock', 'paper', or 'scissors'.");
+  while (input == null) {
+    input = window.prompt("Please choose either 'rock', 'paper', or 'scissors'.");
+  }
+  input = input.toLowerCase();
+  console.log(input);
 }
