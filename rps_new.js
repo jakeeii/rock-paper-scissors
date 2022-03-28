454650135ffb883a66d05db439baf1c8e4409a71
Jake <jakevisc@gmail.com>
2022-03-28 16:36:55 -0230

    Create computer select function
    Created a function that allows the computer to make a random choice
    of either rock, paper, or scissors.

---
+++ rps_new.js
@@ -0,0 +1,12 @@
+//Have computer randomly select either rock, paper, or scissors
+//Get input from player to select rock, paper, or scissors
+//If one person wins, increment their score by one and display who won
+//If there is a tie, do not increment any score and declare a tie
+//Repeat these steps until either player gets a score of 5
+//Display game over and show the winner
+
+const choices = ["rock", "paper", "scissors"]
+
+function computerSelect() {
+  return choices[Math.floor(Math.random() * choices.length)]
+}
